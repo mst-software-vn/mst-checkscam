a170f66383d079e799b15d5baeced4f7648d82e5
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-03-09 02:24:28 +0700

    chore: change size logo with header admin

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -1,10 +1,10 @@
 <div class="header">
     <div class="header-left active">
         <a href="index.html" class="logo">
-            <img src="/assets/img/logo.png" alt="" />
+            <img src="https://i.ibb.co/7xfz0v3K/black.png" alt="" />
         </a>
         <a href="index.html" class="logo-small">
-            <img src="/assets/img/logo-small.png" alt="" />
+            <img src="https://i.ibb.co/d4J0Z7MM/logo.png" alt="" />
         </a>
         <a id="toggle_btn" href="javascript:void(0);"></a>
     </div>
