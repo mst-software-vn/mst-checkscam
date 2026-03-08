1465ad081db124f4f4f576b2bdf857d1d4b8c2ef
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-03-08 22:57:16 +0700

    chore: fix code

--- resources/views/components/ads-horizontal.blade.php
+++ resources/views/components/ads-horizontal.blade.php
@@ -6,6 +6,6 @@
 
 <div class="mx-auto max-w-[950px] overflow-hidden rounded-lg dark:bg-white">
     <a href="{{ $url }}" target="_blank">
-        <img src="{{ $image }}" class="h-12 w-full md:h-18" alt="{{ $alt }}" />
+        <img src="{{ $image }}" class="h-12 w-full md:h-full md:min-h-12" alt="{{ $alt }}" />
     </a>
 </div>
