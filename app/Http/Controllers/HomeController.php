c8ebd4ff0ad56d823e594d0726f6b74c88d1b4a2
maaitlunghau <chunhau.py@gmail.com>
2026-03-11 12:35:48 +0700

    feat(home): optimize home stats display

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends Controller
     private function getLatestReports() {
        return Report::where('status', 'approved')
         ->latest() 
-        ->limit(10)
+        ->limit(5)
         ->get();
     }
 
